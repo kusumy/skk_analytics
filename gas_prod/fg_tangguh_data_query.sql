bf7a609a245c1e284132f8d35417772518cb6168
kusumy <yogiekusumahn@yahoo.com>
2022-11-29 10:53:14 +0700

    oke

---
+++ gas_prod/fg_tangguh_data_query.sql
@@ -0,0 +1,2 @@
+SELECT prod_date AS date, realization_prod AS feed_gas, target_wpnb AS wpnb_gas, is_unplanned_shutdown AS unplanned_shutdown, is_planned_shutdown AS planned_shutdown
+FROM f_lng_feed_gas_bptangguh('BP Tangguh', '20200101', '20221120')
