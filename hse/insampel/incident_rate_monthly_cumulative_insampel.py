432d1e3aad122638e5a5f8902b2ca41649ddc59b
kusumy <yogiekusumahn@yahoo.com>
2022-11-30 14:58:06 +0700

    incident rate monthly cum insampel update

--- hse/insampel/incident_rate_monthly_cumulative_insampel.py
+++ hse/insampel/incident_rate_monthly_cumulative_insampel.py
@@ -118,8 +118,8 @@ def main():
     #Load Data from Database
     #query_1 = open("query_month_cum.sql", mode="rt").read()
     query_1 = open(os.path.join('hse/insampel', 'query_month_cum.sql'), mode="rt").read()
-    #data = get_sql_data(query_1, conn)
-    data = retrieve_data(query_1)
+    data = get_sql_data(query_1, conn)
+    #data = retrieve_data(query_1)
     data['year_num'] = data['year_num'].astype(int)
     data['month_num'] = data['month_num'].astype(int)
     data['date'] = data['year_num'].astype(str) + '-' + data['month_num'].astype(str)
